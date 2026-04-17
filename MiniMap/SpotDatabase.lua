9cf89d1c6ba76abb11afb43c0f064f877c1ef6da
Jango73 <guillaumed00@gmail.com>
2026-04-17 18:49:28 +0200

    Add home spots

--- MiniMap/SpotDatabase.lua
+++ MiniMap/SpotDatabase.lua
@@ -7,6 +7,7 @@ SpotDatabase.__index = SpotDatabase
 RESOURCE_CATEGORIES = {
     { key = 'book', color = { 0.8, 0.3, 0, 1 } },
     { key = 'chest', color = { 1, 0.84, 0, 1 } },
+    { key = 'home', color = { 1, 1, 1, 1 } },
     { key = 'jewelry', color = { 0.9, 0.7, 0.2, 1 } },
     { key = 'ore', color = { 1, 0.5, 0, 1 } },
     { key = 'plant', color = { 0.2, 0.8, 0.2, 1 } },
